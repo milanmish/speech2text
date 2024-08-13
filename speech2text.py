66fb9228bc96dc5cf8737c7435081de0c7a2f4ff
Milan Mishra <milanmishr20@gmail.com>
2024-08-12 23:38:39 -0500

    Text collection
    Speech can now be converted to text

--- speech2text.py
+++ speech2text.py
@@ -1,1 +1,19 @@
 import speech_recognition as sr
+
+recognizer = sr.Recognizer()
+
+with sr.Microphone() as source:
+    print("Checking ambient noise...")
+    recognizer.adjust_for_ambient_noise(source)
+    print("Speak now...")
+    audio = recognizer.listen(source)
+
+
+try:
+    print("Recognizing...")
+    text = recognizer.recognize_google(audio)
+    print("You said:" + text)
+except sr.UnknownValueError:
+    print("Google Speech Recognition could not understand audio")
+except sr.RequestError as e:
+    print(f"Could not request results from Google Speech Recognition service; {e}")
